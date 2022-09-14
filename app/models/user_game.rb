5dae557f443d1bde6f08d56ec277fe02f57e01fd
azranel <bart@chartmogul.com>
2022-09-14 16:42:19 +0200

    Add some simple validations

--- app/models/user_game.rb
+++ app/models/user_game.rb
@@ -1,2 +1,6 @@
 class UserGame < ApplicationRecord
+  validates :iron_resources, :cooper_resources, :gold_resources,
+            numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :iron_factory_level, :copper_factory_level, :gold_factory_level,
+            numericality: { only_integer: true, greater_than_or_equal_to: 1, less_than_or_equal_to: 5 }
 end
