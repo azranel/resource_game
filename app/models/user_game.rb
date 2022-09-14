896474ce06be014b9a3dc39035bd6fd26820f23d
azranel <bart@chartmogul.com>
2022-09-14 17:15:16 +0200

    Fix typos

--- app/models/user_game.rb
+++ app/models/user_game.rb
@@ -1,5 +1,5 @@
 class UserGame < ApplicationRecord
-  validates :iron_resources, :cooper_resources, :gold_resources,
+  validates :iron_resources, :copper_resources, :gold_resources,
             numericality: { only_integer: true, greater_than_or_equal_to: 0 }
   validates :iron_factory_level, :copper_factory_level, :gold_factory_level,
             numericality: { only_integer: true, greater_than_or_equal_to: 1, less_than_or_equal_to: 5 }
